6d20f0754c40c97191e3339cc73aa9de4d3237b5
jack <tathuan96@gmail.com>
2022-05-17 22:29:19 +0700

    Allow overriding thumbnail WITH & HEIGHT

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const {getUserInfo} = require('../data-access/user')
 const {screenshot} = require('../lib/screenshot')
 
+const THUMBNAIL_WIDTH = Number.parseInt(process.env.THUMBNAIL_WIDTH || 1200)
+const THUMBNAIL_HEIGHT = Number.parseInt(process.env.THUMBNAIL_HEIGHT || 630)
+
 router.get('/html/:userId', function (req, res, next) {
     let {userId} = req.params
     let userInfo = getUserInfo(userId);
@@ -12,16 +15,17 @@ router.get('/html/:userId', function (req, res, next) {
 router.get('/thumbnail/:userId.png', function (req, res, next) {
     let {userId} = req.params
     let htmlEndpoint = `http://localhost:${process.env.PORT || 3000}/users/html/${userId}`
-    console.log(htmlEndpoint)
-    screenshot(htmlEndpoint, 1200, 630).then((file) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', `image/png`);
-        res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
-        res.end(file);
-    }).catch(err => {
-        res.statusCode = 500
-        res.end(err.message)
-    });
+    screenshot(htmlEndpoint, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT)
+        .then((file) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', `image/png`);
+            res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+            res.end(file);
+        })
+        .catch(err => {
+            res.statusCode = 500
+            res.end(err.message)
+        });
 });
 
 router.get('/:userId', function (req, res, next) {
