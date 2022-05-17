8d2c6fa69c6a8cd61f7304cd1be6596ab4d86e97
jack <tathuan96@gmail.com>
2022-05-17 22:21:06 +0700

    Update README.MD

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -20,7 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- data-access/user.js
+++ data-access/user.js
@@ -1,10 +1,31 @@
-function getUserInfo(userId) {
-    //=> Mockup data
-    return {
-        id: userId,
+const MOCKUP_RECORDS = [
+    {
+        id: "jack",
         name: "Jack",
         bio: "Hello world, I'm Jack!",
         avatar: "https://avatars.githubusercontent.com/u/19801770?v=4"
+    },
+    {
+        id: "tony",
+        name: "Tony",
+        bio: "Hello world, I'm Tony!",
+        avatar: "https://avatars.githubusercontent.com/u/19801770?v=4"
+    }, {
+        id: "peter",
+        name: "Peter",
+        bio: "Hello world, I'm Peter!",
+        avatar: "https://avatars.githubusercontent.com/u/19801770?v=4"
+    }
+]
+
+function getUserInfo(userId) {
+    let matchingUser = MOCKUP_RECORDS.find(u => u.id === userId)
+    if (matchingUser) return matchingUser
+    else return {
+        id: userId,
+        name: userId,
+        bio: `Hello world, I'm ${userId[0].toUpperCase() + userId.slice(1)}!`,
+        avatar: '/images/default-avatar.png'
     }
 }
 
